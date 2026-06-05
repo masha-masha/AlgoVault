bb1a90f21efa8838243e7525b5e6e4025cac3efe
Maria Bazhenova <yankeevsdixie@gmail.com>
2026-06-05 21:48:50 +0300

    feat: add toggle button for menu

--- src/components/AlgoMenu.tsx
+++ src/components/AlgoMenu.tsx
@@ -1,28 +1,47 @@
+import { useState } from "react";
 import { algorithms } from "../data/algoritms";
-import { useNavigate } from "react-router-dom";
-import { Link } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 
 const AlgorithmMenu = () => {
  const navigate = useNavigate();
+ const [isOpen, setIsOpen] = useState(true);
 
  const handleRandomAlgorithm = () => {
   const randomIndex = Math.floor(Math.random() * algorithms.length);
   const randomAlgorithm = algorithms[randomIndex];
-  navigate(`/algo/${randomAlgorithm.id}`); 
+  navigate(`/algo/${randomAlgorithm.id}`);
  };
+ const toggleMenu = () => {
+  setIsOpen(!isOpen);
+ };
+
  return (
   <aside className="sidebar">
-   <h2>Алгоритмы</h2>
-   <button onClick={handleRandomAlgorithm} className="random-algo-button">
-    Случайный алгоритм
-   </button>
-   <nav>
-    {algorithms.map((algo) => (
-     <Link key={algo.id} to={`/algo/${algo.id}`}>
-      {algo.title}
-     </Link>
-    ))}
-   </nav>
+   <div className="sidebar-header">
+    <h2>
+     Алгоритмы
+     <button onClick={toggleMenu} className="toggle-button">
+      {isOpen ? "▲" : "▼"}
+     </button>
+    </h2>
+   </div>
+
+   {isOpen && (
+    <>
+     {" "}
+     <button onClick={handleRandomAlgorithm} className="random-algo-button">
+      Случайный алгоритм
+     </button>
+     <nav>
+      {algorithms.map((algo) => (
+       <Link key={algo.id} to={`/algo/${algo.id}`}>
+        {" "}
+        {algo.title}
+       </Link>
+      ))}
+     </nav>
+    </>
+   )}
   </aside>
  );
 };
